28eb4cba58be8f37a6f27d5fb88b6c53bcb05c54
pxr687 <pxr687@bham.ac.uk>
2023-11-17 12:10:12 +0000

    add first plots

---
+++ first_analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# import the data
+df = pd.read_csv('data/ckd.csv')
+
+# plot blood pressure as a function of age
+plt.figure()
+plt.scatter(df['Age'], df['Blood Pressure'])
+plt.show()
